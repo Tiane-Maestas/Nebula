ef34fa5187daa6c037c4d2c795da577279ff816d
Tiane Maestas <tianemaestas@gmail.com>
2023-11-15 21:52:11 -0800

    Added MoveTo in Camera2D

--- Camera/CameraController2D.cs
+++ Camera/CameraController2D.cs
@@ -2,22 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public enum CameraState2D { Default, LerpFollow }
+/*
+ * State Ideas:
+ *  - Default -> Never changes and set once.
+ *  - LerpFollow -> Follows an objects transform.
+ *  - MoveTo -> Simply tell camera to move to a specific position that can change.
+ */
+public enum CameraState2D { Default, LerpFollow, MoveTo }
 
 public class CameraController2D : MonoBehaviour
 {
     [SerializeField] private CameraState2D _cameraState = CameraState2D.Default;
     private Camera _camera;
 
-    [Header("Default State")]
+    [Header("'Default' State")]
     [SerializeField] private Vector3 _defaultPosition;
     [SerializeField] private float _defaultOrthographicSize;
     [SerializeField] private float _defaultTransitionSpeed;
 
-    [Header("LerpFollow State")]
+    [Header("'LerpFollow' State")]
     [SerializeField] private Transform _lerpTarget = null;
     private Vector3 _lerpTargetPosition = new Vector3();
     [SerializeField] private float _lerpOrthographicSize;
+    [SerializeField] private float _lerpTransitionSpeed;
+
+    [Header("'MoveTo' State")]
+    [SerializeField] private Vector3 _moveToTargetPosition;
+    [SerializeField] private float _moveToOrthographicSize;
+    [SerializeField] private float _moveToTransitionSpeed;
 
     private void Awake()
     {
@@ -37,17 +49,26 @@ public class CameraController2D : MonoBehaviour
         {
             case CameraState2D.LerpFollow:
                 if (!_lerpTarget) break;
-                _camera.transform.position = Vector3.Lerp(_camera.transform.position, _lerpTargetPosition, _defaultTransitionSpeed * Time.deltaTime);
-                _camera.orthographicSize = Mathf.Lerp(_camera.orthographicSize, _lerpOrthographicSize, _defaultTransitionSpeed * Time.deltaTime);
+                LerpMove(_lerpTargetPosition, _lerpOrthographicSize, _lerpTransitionSpeed);
+                break;
+            case CameraState2D.MoveTo:
+                LerpMove(_moveToTargetPosition, _moveToOrthographicSize, _moveToTransitionSpeed);
                 break;
             default:
-                _camera.transform.position = Vector3.Lerp(_camera.transform.position, _defaultPosition, _defaultTransitionSpeed * Time.deltaTime);
-                _camera.orthographicSize = Mathf.Lerp(_camera.orthographicSize, _defaultOrthographicSize, _defaultTransitionSpeed * Time.deltaTime);
+                LerpMove(_defaultPosition, _defaultOrthographicSize, _defaultTransitionSpeed);
                 break;
         }
+    }
+
+    private void LerpMove(Vector3 position, float size, float speed)
+    {
+        _camera.transform.position = Vector3.Lerp(_camera.transform.position, position, speed * Time.deltaTime);
+        _camera.orthographicSize = Mathf.Lerp(_camera.orthographicSize, size, speed * Time.deltaTime);
     }
 
     public void ChangeState(CameraState2D cameraState) { _cameraState = cameraState; }
 
     public void LerpFollow(Transform lerpTarget) { _lerpTarget = lerpTarget; }
+
+    public void MoveTo(Vector3 moveToTargetPosition, float size) { _moveToTargetPosition = moveToTargetPosition; _moveToOrthographicSize = size; }
 }
