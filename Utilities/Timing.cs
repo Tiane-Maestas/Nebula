aee4469b295f9059e35ad891a598baa0ef3aaa38
Tiane Maestas <tianemaestas@gmail.com>
2024-05-20 20:36:56 -0700

    Added interval wrapper.

--- Utilities/Timing.cs
+++ Utilities/Timing.cs
@@ -7,9 +7,8 @@ namespace Nebula
 {
     public static class Timing
     {
-        // Todo: Allow for callback parameters.
         // 'interval' is float in seconds.
-        public static async Task SetInterval(Action callback, float interval, CancellationToken cancellationToken)
+        public static async Task SetInterval(Action callback, float interval, CancellationToken cancellationToken) // Todo: Allow for callback parameters.
         {
             while (true)
             {
@@ -21,12 +20,25 @@ namespace Nebula
             }
         }
 
-        // Todo: Allow for callback parameters.
         // 'interval' is float in seconds.
-        public static async Task SetTimeout(Action callback, float interval)
+        public static async Task SetTimeout(Action callback, float interval) // Todo: Allow for callback parameters.
         {
             await Task.Delay((int)(interval * 1000));
             callback();
         }
+    }
+
+    public class NebulaTimer
+    {
+        private Task _timerTask = null;
+        private CancellationTokenSource _cancellationTokenSource = new CancellationTokenSource();
+
+        public void SetInterval(Action callback, float interval)
+        {
+            _timerTask = Timing.SetInterval(callback, interval, _cancellationTokenSource.Token);
+        }
+
+        public void Cancel() { _cancellationTokenSource.Cancel(); }
+        ~NebulaTimer() { this.Cancel(); }
     }
 }
