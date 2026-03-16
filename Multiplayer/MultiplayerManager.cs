c403472074a31ee3f60c0cff611f1b567653cd44
Tiane Maestas <tianemaestas@gmail.com>
2026-03-15 20:05:23 -0700

    Added basic switch to change to LAN transport. Needs work to switch back to relay.

--- Multiplayer/MultiplayerManager.cs
+++ Multiplayer/MultiplayerManager.cs
@@ -1,6 +1,8 @@
 using UnityEngine;
 using Unity.Services.Core;
 using Unity.Services.Authentication;
+using Unity.Netcode;
+using Unity.Netcode.Transports.UTP;
 
 namespace Nebula.Multiplayer
 {
@@ -22,5 +24,39 @@ namespace Nebula.Multiplayer
             if (AuthenticationService.Instance.IsSignedIn)
                 AuthenticationService.Instance.SignOut();
         }
+
+        public void StartLanHost() 
+        {
+            NetworkManager.Singleton.Shutdown();
+            ConfigureLanTransport();
+            NetworkManager.Singleton.StartHost();
+        }
+
+        public enum TransportMode
+        {
+            Relay,
+            LAN
+        }
+        private TransportMode _currentTransportMode = TransportMode.Relay;
+
+        private void ConfigureLanTransport()
+        {
+            var transport = NetworkManager.Singleton.GetComponent<UnityTransport>();
+            transport.SetConnectionData(
+                "0.0.0.0",
+                7777
+            );
+
+            _currentTransportMode = TransportMode.LAN;
+        }
+
+        // Todo: Figure out how to go back to relay transport.
+        // private void ConfigureRelayTransport(RelayServerData relayData)
+        // {
+        //     var transport = NetworkManager.Singleton.GetComponent<UnityTransport>();
+        //     transport.SetRelayServerData(relayData);
+
+        //     _currentTransportMode = TransportMode.Relay;
+        // }
     }
 }
